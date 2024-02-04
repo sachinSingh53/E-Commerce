79cd3259c60e73ee00306b88ffc9849a94314de3
sachinSingh53 <20it095@jssaten.ac.in>
2024-02-05 00:11:35 +0530

    implement containerisation using docker

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-const dbUrl=process.env.DB_URL; 
+const dbUrl="mongodb://sachin:sachin@mongo:27017/?authSource=admin"; 
 mongoose.connect(dbUrl,{
 });
 
 const db = mongoose.connection;
 db.on("error",console.error.bind(console,"connection error:"));
 db.once("open",()=>{
-    // console.log("Database connected");
+    console.log("Database connected");
 });
 
 module.exports = db;
